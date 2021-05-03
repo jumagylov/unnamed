c9875f30f62a22cf41c86eae028d6b48975a6475
Ваше Имя <you@example.com>
2021-05-03 16:32:28 +0600

    asdf

--- unnamed_project/settings.py
+++ unnamed_project/settings.py
@@ -141,8 +141,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'akti',
-        'USER': 'asd',
-        'PASSWORD': 'asd',
+        'USER': 'akti',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
